ef6d13a85d708fddddc774eaa71a611538013f3c
ApocryphalWritings <Iamminecraft12@gmail.com>
2019-05-10 21:08:35 -0400

    Added some code, removed others

--- main.py
+++ main.py
@@ -1,19 +1,51 @@
+import asyncio
+import json
+import random
+import logging
+
+
+import aiohttp
 import discord
+from discord import Game
+from discord.ext import commands
+logging.basicConfig(level=logging.INFO)
 
 f = open('Token.txt')
+
 token = f.read()
-class MyClient(discord.Client):
-    async def on_ready(self):
+
+client = commands.Bot(command_prefix = '!')
+
+
+
+
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+async def on_ready(self):
         print('Logged on as', self.user)
 
-    async def on_message(self, message):
-        # don't respond to ourselves
-        if message.author == self.user:
-            return
+@client.event
+async def on_message(message):
+    author = message.author
+    content = message.content
+    print('{}: {}'.format(author, content))
+@client.event
+async def on_message_delete(message):
+    author = message.author
+    content = message.content
+    print('{}: {}'.format(author, content))
+    await message.channel.send('{}: {}'.format(author, content))
+@client.event
+async def on_member_join(self, member):
+        channel = member.guild.system_channel
+        if channel is not None:
+            await channel.send('Welcome {0.mention}.'.format(member))
+            await channel.send.file('welc.jpg')
 
-        if message.content == 'ping':
-            await message.channel.send('pong')
-
-client = MyClient()
 client.run(token)
+
+
 
