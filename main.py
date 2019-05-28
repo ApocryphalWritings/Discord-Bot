83e9a92ef266cbb21a9696caa2ffac4cdf9f587c
ApocryphalWritings <Iamminecraft12@gmail.com>
2019-05-27 23:04:48 -0400

    update #3 actual finally fixed the welcome message and discord presence. more to come soon.

--- main.py
+++ main.py
@@ -1,19 +1,59 @@
+import asyncio
+import json
+import random
+import logging
+
+
+import aiohttp
 import discord
+from discord import Game
+from discord.ext import commands
+logging.basicConfig(level=logging.INFO)
 
 f = open('Token.txt')
+
 token = f.read()
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as', self.user)
 
-    async def on_message(self, message):
-        # don't respond to ourselves
-        if message.author == self.user:
-            return
+client = commands.Bot(command_prefix = '!')
 
-        if message.content == 'ping':
-            await message.channel.send('pong')
 
-client = MyClient()
+
+
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+@client.event
+async def on_ready():
+        
+        print('Logged on as', client.user.name)
+        print(client.user.id)
+        game = discord.Game('With broken code')
+        await client.change_presence(status=discord.Status.idle,activity=game)
+
+@client.event
+async def on_message(message):
+    author = message.author
+    content = message.content
+    print('{}: {}'.format(author, content))
+@client.event
+async def on_message_delete(message):
+    author = message.author
+    content = message.content
+    print('{}: {}'.format(author, content))
+    await message.channel.send('{}: {}'.format(author, content))
+
+
+@client.event
+async def on_member_join(member):
+    server = member.guild
+    channel = client.get_channel(562124177671192600)
+    await channel.send('Welcome {} to {}'.format(member, server))
+    await channel.send(file=discord.File('welc.jpg'))
+    
+    
+
 client.run(token)
+
+#commands
 
